03302a83f37e53fc9f47df332f72a1e3a27032fd
eduramza <eduardo.rmb@outlook.com>
2020-07-26 15:50:49 -0300

    initial config

---
+++ app/src/main/java/com/eduramza/mybraziliexapp/BraziliexApplication.kt
@@ -0,0 +1,6 @@
+package com.eduramza.mybraziliexapp
+
+import android.app.Application
+
+class BraziliexApplication: Application(){
+}
---
+++ app/src/main/java/com/eduramza/mybraziliexapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.eduramza.mybraziliexapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eduramza.mybraziliexapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
